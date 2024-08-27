c45ee342986ae189477d318fdf2d56c2fa95fa4f
Merve <mervealgi@gmail.com>
2024-08-28 01:02:14 +0300

    new samples

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,20 @@ public class Main {
 
         int b = 10;
         double c = 3.0;
-        int d = (int)(a / b);
-        double e = a % b;
-        System.out.println("c:" + c);
+        int d = (int)(b/c);
+        double e = b % c;
         System.out.println("d:" + d);
+        System.out.println("e:" + e);
+
+
+        int f = 5;
+        int g = ++f;
+        int h = f++;
+        int i = --g;
+        System.out.println("f:" + f);
+        System.out.println("g:" + g);
+        System.out.println("h:" + h);
+        System.out.println("i:" + i);
 
 
     }
