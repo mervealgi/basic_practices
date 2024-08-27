2874056dd46388b19b40fa5207d5b501d2dd0d10
Merve Algı <merve.algi@testinium.com>
2024-08-27 19:48:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // int 1value = 1;  //1value is not possible for a variable
+
+        int a = 4;
+        System.out.println(a);
+
+        int b = 10;
+        double c = 3.0;
+        int d = (int)(a / b);
+        double e = a % b;
+        System.out.println("c:" + c);
+        System.out.println("d:" + d);
+
+
+    }
+}
