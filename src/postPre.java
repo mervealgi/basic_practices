1ab0d93322e34d0b82c6ec61470b157974b0004b
Merve <mervealgi@gmail.com>
2024-09-01 19:17:33 +0300

    samples

--- src/postPre.java
+++ src/postPre.java
@@ -1,18 +1,30 @@
 public class postPre {
     public static void main(String[] args) {
         int x = 5;
-        int y = ++x;
-        System.out.println("x: " + x); // x: 6
-        System.out.println("y: " + y); // y: 6
+        int y = ++x;                   // y: 6
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
 
         int a = 5;
-        int b = a++;
+        int b = a++;                   // b: 5
         System.out.println("a: " + a);
         System.out.println("b: " + b);
 
 
+        int c = 7;
+        System.out.println(c--);       // c: 7
+        int d = 1;
+        System.out.println(--d);       // c: 0
 
 
+        int e = 10;
+        int f = 5;
+        int result;
+        result = ++e * f;              // e: 11 * 5
+        System.out.println("Prefix - e: " + e + ", f: " + f + ", result: " + result);
 
+        int g = 10;
+        int result2 = g++ * f;         // g: 10 * 5
+        System.out.println("Postfix - g: " + g + ", f: " + f + ", result: " + result2);
     }
 }
