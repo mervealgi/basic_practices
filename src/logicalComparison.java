ebe367c5a32c5b43f84728940892afaad6902afa
Merve <mervealgi@gmail.com>
2024-09-03 16:23:29 +0300

    logical comparison

--- src/logicalComparison.java
+++ src/logicalComparison.java
@@ -3,12 +3,17 @@ public class logicalComparison {
 
         int n1 = 10;
         int n2 = 11;
-
         System.out.println("Result1 : " + (n1 == n2));
         System.out.println("Result1 : " + (n1 < n2));
         System.out.println("Result1 : " + (n1 > n2));
         System.out.println("Result1 : " + (n1 <= n2));
         System.out.println("Result1 : " + (n1 >= n2));
         System.out.println("Result1 : " + (n1 != n2));
+
+        //
+        boolean v1 = true;
+        boolean v2 = false;
+        System.out.println("value1 and value2: " + (v1 && v2));
+        System.out.println("value1 and value2: " + (v1 || v2));
     }
 }
